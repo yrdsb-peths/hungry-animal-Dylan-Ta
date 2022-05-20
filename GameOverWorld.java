432a7fffbdd1a5ebf713c6577c1d4d67e8c61023
Dylan-Ta <101138961+Dylan-Ta@users.noreply.github.com>
2022-05-20 13:49:45 -0400

    Replay game from gameover screen

--- GameOverWorld.java
+++ GameOverWorld.java
@@ -19,8 +19,16 @@ public class GameOverWorld extends World
         Label gameOverLabel = new Label("Game Over!", 100);
         addObject(gameOverLabel, 300, 200);
 
-        Label scoreLabel = new Label("Score: " + MyWorld.score, 80);
-        addObject(scoreLabel, 280, 280);
+        Label scoreLabel = new Label("Score: " + MyWorld.score, 60);
+        addObject(scoreLabel, 290, 280);
+        Label restartLabel = new Label("Press <space> to restart", 60);
+        addObject(restartLabel, 300, 350);
         MyWorld.score = 0;
+    }
+    public void act(){
+        if(Greenfoot.isKeyDown("space")){
+            MyWorld world = new MyWorld();
+            Greenfoot.setWorld(world);
+        }
     }
 }
